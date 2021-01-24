0cf729db162b45c6c90db212eb4b416987a60aa4
internalinterference <kanequbolt@gmail.com>
2021-01-24 20:22:11 +1000

    adsa

---
+++ MovieApp/Shared/Models/MovieData/MovieDataDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Shared.Models.MovieData
+{
+    public class MovieDataDetail
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Overview { get; set; }
+        public double Popularity { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+    }
+}
---
+++ MovieApp/Shared/Services/TmdbApi.cs
@@ -0,0 +1,32 @@
+﻿using MovieApp.Shared.Models.MovieData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMDbLib.Client;
+
+namespace MovieApp.Shared.Services
+{
+    public class TmdbApi
+    {
+        //TODO: use config
+        private TMDbClient _client = new TMDbClient("dd47404357d7dfca4f753c17e666f789");
+
+        public async Task<MovieDataDetail> GetAllMovieInfo(string title, string lang = "ru")
+        {
+            var results = await _client.SearchMovieAsync(title, lang);
+            var data = results.Results.FirstOrDefault(p => p.Title == title);
+
+            return new MovieDataDetail
+            {
+                Id = data.Id,
+                Title = data.Title,
+                OriginalTitle = data.OriginalTitle,
+                Overview = data.Overview,
+                ReleaseDate = data.ReleaseDate,
+                Popularity = data.Popularity,                
+            };
+        }
+    }
+}
